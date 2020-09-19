d62abb06a921d77a7567285787ba0e9e25be2837
speja969 <62497469+speja969@users.noreply.github.com>
2020-09-19 08:33:24 +0200

    Update Buster_Pejax.sh

--- scripts/Buster_Pejax.sh
+++ scripts/Buster_Pejax.sh
@@ -50,14 +50,14 @@ sudo chmod 777 /usr/share/icons/ncmpcpp_48x48.png
 
 
 # korekcija autorizacije za gdebi
-sudo sed -i 's/<allow_active>auth_admin/<allow_active>yes/' /usr/share/polkit-1/actions/com.ubuntu.pkexec.gdebi-gtk.policy
+#sudo sed -i 's/<allow_active>auth_admin/<allow_active>yes/' /usr/share/polkit-1/actions/com.ubuntu.pkexec.gdebi-gtk.policy
 
 
 # korekcija autorizacije za synaptic
-sudo sed -i 's/<allow_active>auth_admin/<allow_active>yes/' /usr/share/polkit-1/actions/com.ubuntu.pkexec.synaptic.policy
+#sudo sed -i 's/<allow_active>auth_admin/<allow_active>yes/' /usr/share/polkit-1/actions/com.ubuntu.pkexec.synaptic.policy
 
 # korekcija autorizacije za doublecmd
-sudo sed -i 's/<allow_active>auth_admin_keep/<allow_active>yes/' /usr/share/polkit-1/actions/org.doublecmd.root.policy
+#sudo sed -i 's/<allow_active>auth_admin_keep/<allow_active>yes/' /usr/share/polkit-1/actions/org.doublecmd.root.policy
 
 
 sudo chmod --recursive 777 ~/.config/openbox
